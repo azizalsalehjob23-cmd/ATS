beeca7e1d743f6439e10fb77f9b7d9e630467b1e
azizalsalehjob23-cmd <aziz.alsaleh.job23@gmail.com>
2025-08-13 18:22:12 +0300

    Update Card.jsx

--- Card.jsx
+++ Card.jsx
@@ -1,6 +1,17 @@
-export const Card = ({className='', children, ...props}) => (
-  <div className={`card ${className}`} {...props}>{children}</div>
-);
-export const CardContent = ({className='', children, ...props}) => (
-  <div className={`p-4 ${className}`} {...props}>{children}</div>
-);
+import React from "react";
+
+export function Card({ children, className = "", ...props }) {
+  return (
+    <div className={`card ${className}`} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ children, className = "", ...props }) {
+  return (
+    <div className={`p-4 ${className}`} {...props}>
+      {children}
+    </div>
+  );
+}
