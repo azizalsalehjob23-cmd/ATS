45d21af937db3cfbf413429afdbfa6fdd13ab3cd
azizalsalehjob23-cmd <aziz.alsaleh.job23@gmail.com>
2025-08-13 18:22:31 +0300

    Update Input.jsx

--- Input.jsx
+++ Input.jsx
@@ -1,3 +1,17 @@
-export const Input = ({className='', ...props}) => <input className={`input ${className}`} {...props} />;
-export const Textarea = ({className='', ...props}) => <textarea className={`textarea ${className}`} {...props} />;
-export const Label = ({className='', children, ...props}) => <label className={`label ${className}`} {...props}>{children}</label>;
+import React from "react";
+
+export function Input({ className = "", ...props }) {
+  return <input className={`input ${className}`} {...props} />;
+}
+
+export function Textarea({ className = "", rows = 4, ...props }) {
+  return <textarea className={`textarea ${className}`} rows={rows} {...props} />;
+}
+
+export function Label({ className = "", children, ...props }) {
+  return (
+    <label className={`label ${className}`} {...props}>
+      {children}
+    </label>
+  );
+}
