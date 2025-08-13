a0aceeabbe67fe998c593b656eac474822616fb1
azizalsalehjob23-cmd <aziz.alsaleh.job23@gmail.com>
2025-08-13 18:22:57 +0300

    Update Switch.jsx

--- Switch.jsx
+++ Switch.jsx
@@ -1,5 +1,16 @@
-export const Switch = ({checked=false, onCheckedChange=()=>{}, className=''}) => (
-  <button type="button" aria-pressed={checked} className={`switch ${className}`} data-checked={checked} onClick={()=>onCheckedChange(!checked)}>
-    <span className="knob" />
-  </button>
-);
+import React from "react";
+
+export function Switch({ checked, onCheckedChange }) {
+  return (
+    <button
+      type="button"
+      onClick={() => onCheckedChange?.(!checked)}
+      className={`switch-root ${checked ? "bg-indigo-600" : "bg-neutral-300"}`}
+      aria-pressed={checked}
+    >
+      <span
+        className={`switch-thumb ${checked ? "translate-x-5" : "translate-x-1"}`}
+      />
+    </button>
+  );
+}
