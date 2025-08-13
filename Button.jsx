a5ba41ea6c0ecce1421f16d46926af9d0fe851d9
azizalsalehjob23-cmd <aziz.alsaleh.job23@gmail.com>
2025-08-13 18:21:55 +0300

    Update Button.jsx

--- Button.jsx
+++ Button.jsx
@@ -1,3 +1,11 @@
-export const Button = ({className='', variant='primary', children, ...props}) => (
-  <button className={`btn ${variant==='secondary'?'btn-secondary':''} ${className}`} {...props}>{children}</button>
-);
+import React from "react";
+
+export function Button({ children, className = "", variant = "primary", ...props }) {
+  const base = "btn";
+  const styles = variant === "secondary" ? "btn-secondary" : "btn-primary";
+  return (
+    <button className={`${base} ${styles} ${className}`} {...props}>
+      {children}
+    </button>
+  );
+}
