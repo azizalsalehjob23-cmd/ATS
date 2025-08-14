e83c69721e5e1b0dcbaab83f03ef17b8a961d781
azizalsalehjob23-cmd <aziz.alsaleh.job23@gmail.com>
2025-08-14 12:01:36 +0300

    Create api.js

---
+++ api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const fetchJobs = async () => {
+  // هنا تربط مع API حقيقي أو mock
+  return [];
+};
+
+export const fetchCandidates = async (jobId) => {
+  return [];
+};
+
+export const createJob = async (job) => {
+  return {};
+};
+
+export const createCandidate = async (candidate) => {
+  return {};
+};
